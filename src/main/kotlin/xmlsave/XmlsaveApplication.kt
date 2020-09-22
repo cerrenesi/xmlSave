7c6102905eae20febb4a3ada1b746a8dbaba386e
user <stanislav.aleksandrov@excelian.com>
2020-09-22 17:52:03 +0300

    initial commit

---
+++ src/main/kotlin/xmlsave/XmlsaveApplication.kt
@@ -0,0 +1,11 @@
+package xmlsave
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XmlsaveApplication
+
+fun main(args: Array<String>) {
+	runApplication<XmlsaveApplication>(*args)
+}
---
+++ src/test/kotlin/xmlsave/xmlsave/XmlsaveApplicationTests.kt
@@ -0,0 +1,13 @@
+package xmlsave.xmlsave
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XmlsaveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
